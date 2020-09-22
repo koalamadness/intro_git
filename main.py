1419a29dc0d4c93d3d956a046c3fe76b763f64fe
Paul Garcia <paulhiram@hotmail.com>
2020-09-21 22:06:56 -0500

    Pedimos su edad y le agregamos 1

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 nombre = ""
+edad = 0
+
 print("Hola Mundo")
 n = input("Nombre Completo: ")
 nombre = n
 print("Hola " + nombre)
+e = int(input("Edad: "))
+edad = e + 1
+print("Vas a cumplir " + str(edad))
